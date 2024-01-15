858977f0f7537af98220bbaafbaf9c45f0c357f3
minahpark42 <minahpark42@gmail.com>
2024-01-15 16:13:35 +0900

    minah entity finish1

--- backend/src/main/java/com/ssafy/db/entity/Coachings.java
+++ backend/src/main/java/com/ssafy/db/entity/Coachings.java
@@ -1,14 +1,42 @@
 package com.ssafy.db.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+import org.springframework.data.annotation.CreatedDate;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 @Entity
 public class Coachings {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name="coaching_id")
     private String coachingId;
+
+    @ManyToOne
+    @JoinColumn(name="member_id")
+    private Members memberId;
+
+    @Column(name="category_i")
+    private String categoryI;
+
+    @Column(name="category_ii")
+    private String categoryII;
+
+    @Column(name="coaching_name")
+    private String coachingName;
+
+    @Column
+    private Integer price;
+
+    @Column
+    private String description;
+
+    @Column
+    private String summary;
+
+    @CreatedDate
+    @Column(name = "create_date")
+    private LocalDate createDate;
 
 }
