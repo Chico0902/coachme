5f2439808d296861356605dabd94224d630d0443
이준학 <0710june@naver.com>
2024-01-15 16:07:29 +0900

    fix: MemberService 수정

--- backend/src/main/java/com/ssafy/api/service/MemberService.java
+++ backend/src/main/java/com/ssafy/api/service/MemberService.java
@@ -4,13 +4,16 @@ import com.ssafy.api.request.RegistMemberDto;
 import com.ssafy.db.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
+@Transactional(readOnly = true)
 public class MemberService {
 
     private final MemberRepository memberRepository;
 
+    @Transactional
     public void regist(RegistMemberDto registMemberDto) {
 
     }
