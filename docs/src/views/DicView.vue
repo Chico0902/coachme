eb29b870708554ba13b52b4085e2584bf00456a2
Sangyeop Nam <spearoad15@gmail.com>
2024-01-15 09:56:47 +0900

    docs : 프로젝트 문서 vue 추가

---
+++ docs/src/views/DicView.vue
@@ -0,0 +1,119 @@
+<script setup>
+const words = [
+  { name: '코치(coach)', description: '코칭하는 대상, 통상 수입을 창출하기 위해 서비스 사용' },
+  {
+    name: '코미(coame)',
+    description: '코칭받는 대상, 재화를 전문적인 서비스나 영상 정보로 교환하기 위해 서비스 사용'
+  },
+  {
+    name: '관리자(admin)',
+    description: '관리자 페이지 접근권한을 가짐, 코미의 권한 승격이 가능하며 모든 멤버를 관리'
+  },
+  {
+    name: '코칭(coaching)',
+    description: '코치가 제공하는 화상서비스나 녹화영상 등 포괄적으로 포함'
+  },
+  {
+    name: '카테고리(category)',
+    description: '코칭의 종류, Life, Creation, Sports, Develop, Health, Etc로 구분됨.',
+    nested: [
+      {
+        name: 'life',
+        description:
+          '코치의 서비스 종류로 일상에서 만날 수 있는 정보를 제공, House, Furniture, Lifestyle, Design로 구분됨.'
+      },
+      {
+        name: 'Creations',
+        description:
+          '코치의 서비스 종류로 창작/예술 등 생산적인 활동과 관련된 정보 제공, Cooking, Knitting, Art, Beauti로 구분됨.'
+      },
+      {
+        name: 'Sports',
+        description: '코치의 서비스 종류로 스포츠 관련된 정보를 제공, Soccer, Basketball, Tennis, Golf로 구분됨.'
+      },
+      {
+        name: 'Develop',
+        description: '코치의 서비스 종류로 개발 관련된 정보를 제공, Frontend, Backend, Database, Devops로 구분됨.'
+      },
+      {
+        name: 'Health',
+        description: '코치의 서비스 종류로 건강 관련된 정보를 제공, Yoga, Weight, Running, Crossfit로 구분됨.'
+      },
+      {
+        name: 'Etc',
+        description: '코치의 서비스 종류로 기타 서비스들이 포함됨.'
+      }
+    ]
+  },
+  {
+    name: '권한(privilege)',
+    description: '사용자별 다른 권한 부여',
+    nested: [
+      { name: '일반사용자', description: 'lv.0' },
+      { name: '코미', description: 'lv.1' },
+      { name: '코치', description: 'lv.2' },
+      { name: '관리자', description: 'lv.3' }
+    ]
+  },
+  {
+    name: '리스트(--list)',
+    description: '코치, 코미, 코칭 등을 한번에 묶는 개념'
+  },
+  {
+    name: '프로필(profile)',
+    description: '코치나 코미가 본인을 나타내기 위한 개념, 글과 닉네임과 사진으로 구성'
+  },
+  {
+    name: '포트폴리오(portfolio)',
+    description: '코치가 본인을 어필하기 위한 소개, 이력과 쇼츠으로 구성'
+  },
+  { name: '영상(video) ', description: '녹화된 코칭' },
+  { name: '쇼츠(shorts)', description: '코치의 1분 자기소개 어필 영상' },
+  { name: '라이브(live)', description: '코치의 실시간 코칭' },
+  { name: '캘린더(calendar)', description: '코치의 코칭시간 표현, 코미의 코칭시간도 나타냄' },
+  { name: '요약(summary)', description: '코칭에 대한 요약' },
+  { name: '소개페이지(introduce)', description: '코칭에 대한 코치의 자세한 설명' },
+  { name: '공지사항(notice)', description: '코칭에 대한 공지' },
+  { name: '진실의 방(proom)', description: '코칭 시 1:1호출 기능' },
+  { name: '칠판(whiteboard)', description: '코칭 시 공유칠판 기능' },
+  { name: '영상편집(edit)', description: '코칭 영상을 편집하는 기능' },
+  { name: 'DM', description: '1:1 채팅기능' },
+  { name: '찜콩(like)', description: '관심가는 코치나 코칭에 대한 표시' },
+  { name: '알람(notification)', description: '코칭 시작알람, 찜콩 여부, DM 등 발생 알림' },
+  { name: '리뷰(review)', description: '코칭이나 코치에 대한 평가' },
+  { name: '별점(score)', description: '코칭이나 코치에 대한 평가 지표' }
+]
+</script>
+<template>
+  <div id="dic">
+    <h1 class="mt-3 mb-3">용어사전(Dictionary)</h1>
+    <hr />
+    <!-- table -->
+    <table class="table">
+      <thead>
+        <th scope="col">번호</th>
+        <th scope="col">이름</th>
+        <th scope="col">설명</th>
+      </thead>
+      <tbody v-for="(word, index) in words" :key="index">
+        <tr>
+          <td class="text-center">{{ index + 1 }}</td>
+          <td>{{ word.name }}</td>
+          <td>{{ word.description }}</td>
+        </tr>
+        <template v-if="word.nested != undefined">
+          <tr v-for="(nestedWord, index2) in word.nested" :key="index2">
+            <td colspan="2">
+              <span class="ms-5">
+                {{ nestedWord.name }}
+              </span>
+            </td>
+            <td>
+              <span class="ms-3">{{ nestedWord.description }}</span>
+            </td>
+          </tr>
+        </template>
+      </tbody>
+    </table>
+  </div>
+</template>
---
+++ docs/src/views/SpecView.vue
@@ -0,0 +1,293 @@
+<script setup>
+import SideIndex from '../components/SideIndex.vue'
+const indexes = [
+  { id: 'spec-1', title: '계정 관리 기능' },
+  { id: 'spec-2', title: '관리자 기능' },
+  { id: 'spec-3', title: '코치 정보 관리기능' },
+  { id: 'spec-4', title: '채팅(DM) 기능' },
+  { id: 'spec-5', title: '코칭 기능' },
+  { id: 'spec-6', title: '라이브 코칭 기능' },
+  { id: 'spec-7', title: '영상 코칭 기능' },
+  { id: 'spec-8', title: '찜콩 기능' },
+  { id: 'spec-9', title: '리뷰 관리 기능' },
+  { id: 'spec-10', title: '수익 관리 기능' }
+]
+const specs = [
+  {
+    title: '계정(member) 관리 기능',
+    stories: [
+      { category: '명세 필수', description: '방문자(visitor)는 메인페이지를 볼 수 있어야 한다.' },
+      { category: '명세 필수', description: '방문자(visitor)는 로그인 할 수 있어야 한다.' },
+      { category: '명세 필수', description: '방문자(visitor)는 회원가입할 수 있어야 한다.' },
+      { category: '명세 필수', description: '방문자(visitor)는 비밀번호를 찾을 수 있어야 한다.' },
+      {
+        category: '핵심 부가',
+        description: '코미(coame)는 코치(coach)로 권한(privilege)상승을 요청할 수 있어야 한다.'
+      },
+      {
+        category: '핵심 부가',
+        description: '코미(coame)는 기본 회원정보를 조회 및 수정할 수 있어야 한다.'
+      },
+      {
+        category: '핵심 부가',
+        description: '코미(coame)는 프로필(profile)을 조회, 수정, 삭제할 수 있어야 한다.'
+      }
+    ]
+  },
+  {
+    title: '관리자(admin) 기능',
+    stories: [
+      {
+        category: '핵심 부가',
+        description: '관리자(admin)는 사용자 리스트(list)를 조회할 수 있어야 한다.'
+      },
+      {
+        category: '핵심 부가',
+        description: '관리자(admin)는 권한(privilege) 상승리스트(list)를 확인할 수 있어야 한다.'
+      },
+      {
+        category: '핵심 부가',
+        description: '관리자(admin)는 권한(privilege)을 변경할 수 있어야 한다.'
+      }
+    ]
+  },
+  {
+    title: '코치(coach) 정보 관리기능',
+    stories: [
+      {
+        category: '핵심 부가',
+        description: '코미(coame)는 코치(coach) 리스트(list)를 확인해서 원하는 코치(coach)를 선택할 수 있어야 한다.'
+      },
+      {
+        category: '핵심 부가',
+        description: '코치(coach)는 포트폴리오(portfolio)를 작성할 수 있어야 한다.'
+      },
+      {
+        category: '핵심 부가',
+        description: '코치(coach)는 소개영상(video)을 관리할 수 있어야 한다.'
+      }
+    ]
+  },
+  {
+    title: '채팅(DM) 기능',
+    stories: [
+      {
+        category: '명세 필수',
+        description: '코미(coame)는 코치(coach)에게 DM을 보낼 수 있어야 한다.'
+      },
+      {
+        category: '명세 필수',
+        description: '코치(coach)와 코미(coame)는 DM 리스트(list)를 조회 및 관리할 수 있어야 한다.'
+      }
+    ]
+  },
+  {
+    title: '코칭(coaching) 기능',
+    stories: [
+      {
+        category: '명세 필수',
+        description:
+          '코미(coame)는 코칭(coaching) 리스트(list)를 확인해서 원하는 코칭(coaching)을 선택할 수 있어야 한다.'
+      },
+      {
+        category: '명세 필수',
+        description:
+          '코치(coach)는 해당 코칭(coaching)을 신청한 코미(coame) 리스트(list)를 확인 및 관리할 수 있어야 한다.'
+      },
+      {
+        category: '명세 필수',
+        description: '코미(coame)는 마이페이지에서 계획된 코칭(coaching) 리스트(list)를 볼 수 있어야 한다.'
+      }
+    ]
+  },
+  {
+    title: '라이브(live) 코칭(coaching) 기능',
+    stories: [
+      {
+        category: '명세 필수',
+        description: '코치(coach)는 라이브(live) 코칭(coaching)을 개설할 수 있어야 한다.'
+      },
+      {
+        category: '명세 필수',
+        description: '코미(coame)는 라이브(live) 코칭(coaching)에 참가할 수 있어야 한다.'
+      },
+      {
+        category: '명세 필수',
+        description: '코미(coame)는 라이브(live) 채팅 시 코치(coach)와 화상채팅을 할 수 있어야 한다.'
+      },
+      {
+        category: '핵심 부가',
+        description: '코치(coach)는 코칭(coaching)을 녹화해서 영상(video)으로 만들 수 있어야 한다.'
+      },
+      {
+        category: '기타 부가',
+        description:
+          '코치(coach)는 코칭(coaching)시 특정 코미(coame)를 선택해서 진실의 방(proom)에 초대할 수 있어야 한다.'
+      },
+      {
+        category: '기타 부가',
+        description: '코치(coach)와 코미(coame)는 칠판(whiteboard)을 사용할 수 있어야 한다.'
+      }
+    ]
+  },
+  {
+    title: '영상(video) 코칭(coaching) 기능',
+    stories: [
+      {
+        category: '기타 부가',
+        description: '코치(coach)는 코칭(coaching) 영상(video)을 등록, 삭제할 수 있어야 한다.'
+      },
+      {
+        category: '기타 부가',
+        description: '코치(coach)는 코칭(coaching) 영상(video)을 편집(edit)할 수 있어야 한다.'
+      },
+      {
+        category: '기타 부가',
+        description: '코치(coach)는 코칭(coaching) 영상(video)을 AI를 통해 빠르게 편집(edit)할 수 있어야 한다.'
+      }
+    ]
+  },
+  {
+    title: '찜콩(like) 기능',
+    stories: [
+      {
+        category: '핵심 부가',
+        description: '코미(coame)는 원하는 코치(coach)나 코칭(coaching)을 찜콩(like)할 수 있어야 한다.'
+      },
+      {
+        category: '핵심 부가',
+        description: '코미(coame)는 마이페이지에서 찜콩(like)한 수강리스트(list)를 확인할 수 있어야 한다.'
+      },
+      {
+        category: '핵심 부가',
+        description: '찜콩(like) 시 해당 코치(coach)에게 알람(notification)이 발생해야 한다.'
+      }
+    ]
+  },
+  {
+    title: '리뷰(review) 관리 기능',
+    stories: [
+      {
+        category: '핵심 부가',
+        description: ' 코미(coame)는 코치(coach)나 코칭(coame)의 글과 별점(score)를 남길 수 있어야 한다.'
+      },
+      {
+        category: '핵심 부가',
+        description: '코미(coame)는 본인의 리뷰(review)와 별점(score)을 관리할 수 있어야 한다.'
+      },
+      {
+        category: '핵심 부가',
+        description: '코치(coach)는 리뷰(review)를 조회할 수 있어야 한다.'
+      }
+    ]
+  },
+  {
+    title: '수익 관리 기능',
+    stories: [
+      {
+        category: '기타 부가',
+        description: '코미(coame)는 결제내역을 확인할 수 있어야 한다.'
+      },
+      {
+        category: '기타 부가',
+        description: '코치(coach)는 현재까지의 수익을 확인할 수 있어야 한다.'
+      }
+    ]
+  }
+]
+</script>
+<template>
+  <div id="spec">
+    <h1 class="mt-3 mb-3">명세서(Specification)</h1>
+    <SideIndex :indexes="indexes" />
+    <hr />
+    <!-- index -->
+    <h2>Index</h2>
+    <ol>
+      <h4>
+        <li class="p-2">
+          <a href="#spec-1">계정(member) 관리 기능</a>
+        </li>
+      </h4>
+      <h4>
+        <li class="p-2">
+          <a href="#spec-2">관리자(admin) 기능</a>
+        </li>
+      </h4>
+      <h4>
+        <li class="p-2">
+          <a href="#spec-3">코치(coach) 정보 관리기능</a>
+        </li>
+      </h4>
+      <h4>
+        <li class="p-2">
+          <a href="#spec-4">채팅(DM) 기능</a>
+        </li>
+      </h4>
+      <h4>
+        <li class="p-2">
+          <a href="#spec-5">코칭(coaching) 기능</a>
+        </li>
+      </h4>
+      <h4>
+        <li class="p-2">
+          <a href="#spec-6">라이브(live) 코칭(coaching) 기능</a>
+        </li>
+      </h4>
+      <h4>
+        <li class="p-2">
+          <a href="#spec-7">영상(video) 코칭(coaching) 기능</a>
+        </li>
+      </h4>
+      <h4>
+        <li class="p-2">
+          <a href="#spec-8">찜콩(like) 기능</a>
+        </li>
+      </h4>
+      <h4>
+        <li class="p-2">
+          <a href="#spec-9">리뷰(review) 관리 기능</a>
+        </li>
+      </h4>
+      <h4>
+        <li class="p-2">
+          <a href="#spec-10">수익 관리 기능</a>
+        </li>
+      </h4>
+    </ol>
+    <hr />
+    <!-- table -->
+    <table class="table">
+      <template v-for="(spec, index) in specs" :key="index">
+        <thead>
+          <tr>
+            <td colspan="4">
+              <h4 :id="`spec-${index + 1}`">{{ index + 1 }}. {{ spec.title }}</h4>
+            </td>
+          </tr>
+          <tr>
+            <th scope="col">번호</th>
+            <th scope="col">분류</th>
+            <th scope="col">스토리</th>
+          </tr>
+        </thead>
+        <tbody v-for="(story, index2) in spec.stories" :key="index2">
+          <tr>
+            <td class="text-center" :id="`spec-${index + 1}-${index2 + 1}`">{{ index2 + 1 }}</td>
+            <td>{{ story.category }}</td>
+            <td>{{ story.description }}</td>
+          </tr>
+        </tbody>
+        <td>&nbsp;</td>
+      </template>
+    </table>
+  </div>
+</template>
+<style scoped>
+#spec {
+  width: 75%;
+}
+a {
+  text-decoration: none;
+}
+</style>
