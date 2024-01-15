68acc15fa507c307f7f8d3e1f2feff85978b1b75
김지원 <wldnjs35242@naver.com>
2024-01-15 15:22:07 +0900

    feat: 생성일, 수정일 LocalDate annotation 추가

--- backend/src/main/java/com/ssafy/db/entity/Files.java
+++ backend/src/main/java/com/ssafy/db/entity/Files.java
@@ -1,6 +1,10 @@
 package com.ssafy.db.entity;
 
 import jakarta.persistence.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import java.time.LocalDate;
 
 @Entity
 @Table(name="Files")
@@ -20,6 +24,8 @@ public class Files {
     private String name;
     private String url;
     private String type;
-    private String modifyDate;
-    private String createDate;
+    @LastModifiedDate
+    private LocalDate modifyDate;
+    @CreatedDate
+    private LocalDate createDate;
 }
