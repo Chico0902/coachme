4f89fc4b8976969df30d2611f79c41f0bf7de125
이준학 <0710june@naver.com>
2024-01-15 16:05:36 +0900

    fix: MemberRepository 수정

--- backend/src/main/java/com/ssafy/db/repository/MemberRepository.java
+++ backend/src/main/java/com/ssafy/db/repository/MemberRepository.java
@@ -1,8 +1,14 @@
 package com.ssafy.db.repository;
 
+import jakarta.persistence.EntityManager;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
 @Repository
+@RequiredArgsConstructor
 public class MemberRepository {
+
+    private final EntityManager em;
+
 
 }
