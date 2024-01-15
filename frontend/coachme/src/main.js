7ac164c3317be32514b87aa9aef5b0c95d013527
HyejiLee <lee02g29@naver.com>
2024-01-15 14:04:17 +0900

    S10P12A403-229 feat: quasar 기능 추가

--- frontend/coachme/src/main.js
+++ frontend/coachme/src/main.js
@@ -1,14 +1,20 @@
 import './assets/main.css'
 
 import { createApp } from 'vue'
+import { Quasar } from 'quasar'
 import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
+import '@quasar/extras/material-icons/material-icons.css'
+import 'quasar/dist/quasar.css'
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.use(Quasar, {
+    plugins: {}
+})
 
 app.mount('#app')
--- frontend/coachme/vite.config.js
+++ frontend/coachme/vite.config.js
@@ -2,11 +2,15 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { quasar, transformAssetUrls } from '@quasar/vite-plugin'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    vue(),
+    vue({
+      template: {transformAssetUrls}
+    }),
+    quasar()
   ],
   resolve: {
     alias: {
