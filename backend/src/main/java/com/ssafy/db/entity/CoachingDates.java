d8a722323e2cf0fb3c63e2ace086eeb0ce30d3f5
minahpark42 <minahpark42@gmail.com>
2024-01-15 15:09:36 +0900

    coachingDates file name edit

--- backend/src/main/java/com/ssafy/db/entity/CoachingDates.java
+++ backend/src/main/java/com/ssafy/db/entity/CoachingDates.java
@@ -6,7 +6,7 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
 @Entity
-public class Coaching_Dates {
+public class CoachingDates {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Integer coachingDateId;
