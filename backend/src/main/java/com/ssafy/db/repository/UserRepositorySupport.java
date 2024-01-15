89c3dbca144b671e151b5de2b8a4671072e5a6d6
이준학 <0710june@naver.com>
2024-01-15 10:17:54 +0900

    feat: base 프로젝트 생성

---
+++ backend/src/main/java/com/ssafy/db/repository/UserRepositorySupport.java
@@ -0,0 +1,11 @@
+package com.ssafy.db.repository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class UserRepositorySupport {
+
+    @Autowired
+    UserRepository userRepository;
+
+
+}
