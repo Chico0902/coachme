a993ebc5213c99b0030ef347fa7984eda537dc9b
이준학 <0710june@naver.com>
2024-01-15 15:20:30 +0900

    fix: 엔터티 작성 완료

--- backend/src/main/java/com/ssafy/db/entity/Portfolios.java
+++ backend/src/main/java/com/ssafy/db/entity/Portfolios.java
@@ -4,6 +4,7 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import org.springframework.data.annotation.CreatedDate;
 
 import java.time.LocalDateTime;
 
@@ -16,6 +17,7 @@ public class Portfolios {
     @Column
     private String description;
 
+    @CreatedDate
     @Column
     private LocalDateTime create_date;
 }
