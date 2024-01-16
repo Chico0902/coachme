ccde2f3f181e296d966651cf0161e0e34a6bc441
HyejiLee <lee02g29@naver.com>
2024-01-16 16:53:48 +0900

    feat : 캐러셀, 드롭다운, 찜콩, 프로필 사진, 검색 컴포넌트 추가

--- frontend/coachme/src/components/atoms/button.vue
+++ frontend/coachme/src/components/atoms/button.vue
@@ -1,46 +1,44 @@
-<!-- 버튼 컴포넌트 
+<!-- button 컴포넌트 
 필요한 정보 : 라벨, 크기, 버튼 색, 폰트색
 label : 버튼위에 작성될 라벨. 문자열. 기본값 "버튼"
-size : 버튼의 크기. 숫자만. 기본값 20(px)
-background : 색상코드를 문자열로. 기본값 #FCBF17
-color : 색상코드를 문자열로. 기본값 #FFFFFF
+size : 버튼의 라벨 글자 크기. 문자열. 기본값 없음. 추천값 : px, rem, (xs|sm|md|lg|xl) 
+background : 버튼 색상. 색상코드를 문자열로. 기본값 #FCBF17
+color : 글자 색상. 색상코드를 문자열로. 기본값 #FFFFFF
 -->
 
 <script setup>
 import { reactive } from "vue";
 
 const props = defineProps({
-    label: {  // 버튼 라벨
-        type: String,
-        default: "버튼"
-    },
+  label: {  // 버튼 라벨
+    type: String,
+    default: "버튼"
+  },
 
-    size: { // 버튼 크기로 vh단위이며, 버튼은 높이에 비례하는 크기를 가짐
-        type: Number,
-        default: 20
-    }, 
-    background: {
-        type : String,
-        default : "#FCBF17"
-    },
-    color: {
-        type: String,
-        default : "#FFFFFF"
-    },
+  size: { // 버튼 크기
+    type: String,
+    default: ""
+  },
+  background: {
+    type: String,
+    default: "#FCBF17"
+  },
+  color: {
+    type: String,
+    default: "#FFFFFF"
+  },
 });
 
 const styleObject = reactive({
-    background: props.background,
-    color: props.color
+  background: props.background,
+  color: props.color
 }) // 인라인 스타일문, 버튼 스타일에 적용
 
 </script>
 
 <template>
-    <div class="q-pa-md q-gutter-sm">
-        <q-btn :size="`${props.size}px`" :style="styleObject" :label="`${props.label}`">
-        </q-btn>
-    </div>
+  <q-btn :size="`${props.size}`" :style="styleObject" :label="`${props.label}`">
+  </q-btn>
 </template>
 
 <style scoped></style>
--- frontend/coachme/src/components/atoms/dropdown.vue
+++ frontend/coachme/src/components/atoms/dropdown.vue
@@ -1,38 +1,54 @@
+<!-- dropdown 컴포넌트 
+필요한 정보 : 라벨, 색상, 크기, 메뉴 리스트
+label : 드릅다운 메뉴 맨 위에 표시되는 문구. 문자열. 초기값 "선택"
+color : 드롭다운 메뉴 색상. 문자열. 초기값 : "blue-10". 그 외에 추천 값 "amber-7" 프로젝트 색상 기준
+그 외에는 색상표 참고 : https://quasar.dev/style/color-palette/
+size : 드롭다운의 글자 크기. 문자열. 기본값 없음. 추천값 : px, rem, (xs|sm|md|lg|xl) 
+list : 드롭다운 클릭시 들어갈 메뉴들. 리스트. 기본값 ["A","B","C"]
+-->
+
 <script setup>
+const props = defineProps({
+  label : {  
+    type : String,
+    default : "선택"
+  },
+  color : {
+    type : String,
+    default : "blue-10"
+  },
+  size : {
+    type : String,
+    default : ""
+  },
+  list: {
+    type : Object,   
+    default: () => {
+      return ["A", "B", "C"]
+    }
+  }
+});
+
+const emit = defineEmits(['clickMenu'])
+
+const clickEvent = (menu) => {
+  emit('clickMenu', menu)
+}
 
 </script>
 
 <template>
-    <div class="q-pa-md">
-    <q-btn-dropdown
-      split
-      to="/start/pick-quasar-flavour"
-      color="teal"
-      label="Go to Docs Index"
-    >
+    <q-btn-dropdown to="/start/pick-quasar-flavour" :color="`${props.color}`" :label="`${props.label}`" :size="`${props.size}`">
       <q-list>
-        <q-item clickable v-close-popup>
-          <q-item-section>
-            <q-item-label>Photos</q-item-label>
-          </q-item-section>
-        </q-item>
-
-        <q-item clickable v-close-popup>
-          <q-item-section>
-            <q-item-label>Videos</q-item-label>
-          </q-item-section>
-        </q-item>
-
-        <q-item clickable v-close-popup>
-          <q-item-section>
-            <q-item-label>Articles</q-item-label>
-          </q-item-section>
-        </q-item>
+        <div v-for="menu in props.list" :key="menu">
+          <q-item clickable v-close-popup>
+            <q-item-section @click=clickEvent(menu)>
+              <q-item-label>{{ menu }}</q-item-label>
+            </q-item-section>
+          </q-item>
+        </div>
       </q-list>
     </q-btn-dropdown>
-  </div>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
