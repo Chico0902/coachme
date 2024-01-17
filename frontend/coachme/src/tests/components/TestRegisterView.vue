35b89a90ad77199fe171257cc8b5594b3ae5b681
Sangyeop Nam <spearoad15@gmail.com>
2024-01-17 16:34:51 +0900

    refact : 로그인 util function member에 통합 - api 스펙에 맞게 function을 사용하기 위해 api별로 function 생성하는 것으로 변경

---
+++ frontend/coachme/src/tests/components/TestRegisterView.vue
@@ -0,0 +1,38 @@
+<script setup>
+import { ref } from 'vue'
+
+// variables
+const id = ref('')
+const pw = ref('')
+const name = ref('')
+const nick = ref('')
+const email = ref('')
+
+// methods
+const regist = async (id, pw, name, nick, email) => {}
+</script>
+<template>
+  <div class="q-pa-md">
+    <form @submit.prevent="regist(id, pw, name, nick, email)">
+      <div class="q-gutter-md" style="max-width: 500px">
+        <q-input standout="bg-teal text-white" v-model="id" label="아이디" maxlength="20" />
+        <q-input
+          standout="bg-teal text-white"
+          type="password"
+          v-model="pw"
+          label="비밀번호"
+          maxlength="30"
+        />
+        <q-input standout="bg-teal text-white" v-model="name" label="이름" maxlength="10" />
+        <q-input standout="bg-teal text-white" v-model="nick" label="닉네임" maxlength="10" />
+        <q-input
+          standout="bg-teal text-white"
+          type="email"
+          v-model="email"
+          label="이메일"
+          maxlength="50"
+        />
+      </div>
+    </form>
+  </div>
+</template>
