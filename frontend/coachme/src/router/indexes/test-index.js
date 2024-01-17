3c3fac1a35b5eb3042d825dd5b6e26bdb0897f7f
Sangyeop Nam <spearoad15@gmail.com>
2024-01-17 16:42:40 +0900

    S10P12A403-340 feat : 회원가입 테스트 컴포넌트 생성 - 회원가입 테스트를 위한 컴포넌트 생성 - 이제 개발자는 테스트 컴포넌트로 API를 테스트할 수 있음.

--- frontend/coachme/src/router/indexes/test-index.js
+++ frontend/coachme/src/router/indexes/test-index.js
@@ -1,8 +1,14 @@
 import TestLoginView from '../../tests/components/TestLoginView.vue'
+import TestRegistView from '../../tests/components/TestRegistView.vue'
 export default [
   {
     path: '/test/login',
     name: 'login-test',
     component: TestLoginView
+  },
+  {
+    path: '/test/regist',
+    name: 'regist-test',
+    component: TestRegistView
   }
 ]
