7874516049a48d3aadd6e9836ad66f8323b4735a
KDH <silverstuff@naver.com>
2024-01-17 17:45:36 +0900

    feat 메인페이지 layout 작성 시작

--- frontend/coachme/src/layout/MainLayout.vue
+++ frontend/coachme/src/layout/MainLayout.vue
@@ -1,86 +1,117 @@
 <!-- layout/MainLayout.vue -->
 
 <template>
-  <div class = "all">
+  <div class="all">
     <div class="main-layout">
-      <div class ="nav-bar">
+      <div class="nav-bar">
         <p>Navigation Bar</p>
       </div>
-      <div class="crusell">
-        크루셀
+      <div class="crusell-outside">
+        <div class="crusell">크루셀</div>
       </div>
-      
-      <div class="search">
-        <div class="title">
-          타이틀
+      <div class="search-outside">
+        <div class="search">
+          <div class="search-title">타이틀</div>
+          검색창
         </div>
-        검색창
       </div>
-      <div>
+      <div class="category-outside">
+        <div class="category">
+          <div class="category-title">카테고리 문구</div>
+          카테고리 이모지
+        </div>
+      </div>
+      <div class="coach">
+        코치
+        <div class="coach-detail">코치카드</div>
+        <div class="coach-detail">코치카드</div>
+        <div class="coach-detail">코치카드</div>
       </div>
     </div>
   </div>
 </template>
 
 <script>
-export default {
-  // 필요한 경우 데이터, 메서드 등을 추가할 수 있습니다.
-};
+export default {}
 </script>
 
 <style scoped>
-/* 스타일 추가 */
-
-.all{
+.all {
   display: flex;
   justify-content: center;
 }
 .main-layout {
   width: 1280px;
-  height:100vh;
-  background: #D9D9D9;
-  display: flex;
+  min-height: 100vh;
+  background: #d9d9d9;
   justify-content: center;
+  align-items: flex-start;
   overflow-y: auto;
-
+  position: relative;
 }
 
-.nav-bar{
-  width: 1146px;
+.nav-bar {
+  width: 100%;
   height: 50px;
-  position: absolute;
-  right: 112px;
-  top: 32px;
+  margin-bottom: 100px;
+
+  background-color: aqua;
   justify-content: center;
   text-align: center;
+}
+
+.crusell-outside {
+  display: flex;
+  justify-content: center;
 }
 
 .crusell {
   width: 1150px;
   height: 400px;
-  position: absolute;
-  left: 128px;
-  top: 215px;
   flex-shrink: 0;
   border-radius: 45px;
-  background: #FFF;
+  background: #fff;
   justify-content: center;
   text-align: center;
+  margin: 50px;
 }
 
-.title{
-  width: 315px;
+.search-title {
+  width: 100%;
   height: 71px;
-
+  background-color: tomato;
+  /* display: block; */
 }
 
-.search{
+.search-outside {
+  display: flex;
+  justify-content: center;
+  margin: 50px;
+}
+.search {
   width: 821px;
   height: 290px;
-  position: absolute;
-  right: 281px;
-  top: 697px;
-
+  
+ 
+  background-color: skyblue;
+ 
+  
 }
-/* 나머지 스타일들을 필요에 따라 추가 */
+
+.category-outside {
+  display: flex;
+  justify-content: center;
+}
+.category {
+  width: 100%;
+  height: 71px;
+  background-color: tomato;
+  /* display: block; */
+}
+
+.category-title {
+  width: 30%;
+  height: 80px;
+  background-color: yellow;
+}
 </style>
