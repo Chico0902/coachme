cb59ea0390913538aeb05ca9d89c1a0818999032
Sangyeop Nam <spearoad15@gmail.com>
2024-01-18 15:58:11 +0900

    S10P12A403-332 feat : 프로필 사진 테스트 컴포넌트 생성 - 프로필 사진 업로드 및 글씨 변경을 위한 테스트 컴포넌트 생성 - 이제 개발자는 사진 업로드 및 프로필 변경 기능을 테스트할 수 있음

---
+++ frontend/coachme/src/tests/components/TestProfileTextUpload.vue
@@ -0,0 +1,24 @@
+<script setup>
+import { ref } from 'vue'
+
+const profileText = ref('')
+</script>
+<template>
+  <q-input
+    v-model="profileText"
+    filled
+    clearable
+    color="purple-12"
+    label="프로필 글을 입력하세요."
+    hint="프로필 글은 50자까지 입력 가능합니다."
+    maxlength="50"
+  >
+    <template v-slot:append>
+      <q-icon v-if="profileText == ''" name="draw" />
+    </template>
+
+    <template v-slot:after>
+      <q-btn round dense flat icon="send" @click="$emit('changeTextEmit')" />
+    </template>
+  </q-input>
+</template>
