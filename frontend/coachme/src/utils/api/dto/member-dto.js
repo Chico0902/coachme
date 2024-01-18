be97b7bbbe7f15f7b0ce1072d6935fdf4941c5cd
Sangyeop Nam <spearoad15@gmail.com>
2024-01-18 13:56:50 +0900

    S10P12A403-330 feat : 프로필 글, 사진 API 구현 - 백엔드로 프로필 글과 사진을 요청하는 API 구현 - 이제 사용자는 본인의 프로필 글과 사진을 조회할 수 있음.

--- frontend/coachme/src/utils/api/dto/member-dto.js
+++ frontend/coachme/src/utils/api/dto/member-dto.js
@@ -32,7 +32,7 @@ export class MemberRegistRequestDto {
 
 export class changePasswordRequestDto {
   /**
-   * API번호 : member-2
+   * API번호 : member-3
    * 비밀번호 변경 요청을 위한 DTO 클래스
    * @param {*} id
    * @param {*} email
@@ -40,5 +40,27 @@ export class changePasswordRequestDto {
   constructor(id, email) {
     this.id = id
     this.email = email
+  }
+}
+
+export class ProfileTextRequestDto {
+  /**
+   * API번호 : member-8, 10
+   * 프로필 글 등록 및 수정 요청을 위한 DTO 클래스
+   * @param {String} profileText 프로필 글
+   */
+  constructor(profileText) {
+    this.profileText = profileText
+  }
+}
+
+export class ProfileImageRequestDto {
+  /**
+   * API번호 : member-9, 11
+   * 프로필 사진 등록 및 수정 요청을 위한 DTO 클래스
+   * @param {FormData} profileImage 프로필 사진
+   */
+  constructor(profileImage) {
+    this.profileImage = profileImage
   }
 }
