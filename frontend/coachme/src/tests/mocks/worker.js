a1ce655c8813d58a5379037a893b574c2947e23d
남상엽 <spearoad15@gmail.com>
2024-01-18 17:19:09 +0900

    S10P12A403-249-계정관리기능-방문자-visitor-는-비밀번호를-찾을-수-있어야-한다/feat/2

--- frontend/coachme/src/tests/mocks/worker.js
+++ frontend/coachme/src/tests/mocks/worker.js
@@ -2,8 +2,6 @@ import { setupWorker } from 'msw/browser'
 import { HttpResponse, http } from 'msw'
 
 const mockLoginResponseDto = {
-  memberId: '123',
-  privilege: '1',
   accessToken: 'mock-access-token',
   refreshToken: 'mock-refresh-token'
 }
@@ -11,12 +9,20 @@ const mockLoginResponseDto = {
 const mockMemberRegistResponseDto = {
   message: '회원가입 성공'
 }
+
+const mockchangePasswordResponseDto = {
+  message: '비밀번호 변경 및 임시 비밀번호 발송 완료'
+}
+
 const handlers = [
   http.post('http://localhost/members/login', () => {
     return HttpResponse.json(mockLoginResponseDto)
   }),
   http.post('http://localhost/members', () => {
     return HttpResponse.json(mockMemberRegistResponseDto)
+  }),
+  http.patch('http://localhost/members/passwords', () => {
+    return HttpResponse.json(mockchangePasswordResponseDto)
   })
 ]
 
