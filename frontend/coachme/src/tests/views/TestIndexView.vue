71907e2bf414807dbe7b876703177726e4f821d9
남상엽 <spearoad15@gmail.com>
2024-01-18 17:16:20 +0900

    S10P12A403-268-계정관리기능-방문자-visitor-는-회원가입할-수-있어야-한다/feat/2

---
+++ frontend/coachme/src/tests/views/TestIndexView.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="q-ma-lg">
+    <h3 class="mt-5">테스트를 위한 Index Page</h3>
+    <div class="q-pa-md q-gutter-sm">
+      <RouterLink :to="{ name: 'test-login' }">
+        <q-btn color="white" text-color="black" label="로그인" />
+      </RouterLink>
+      <RouterLink :to="{ name: 'test-regist' }">
+        <q-btn color="primary" label="회원가입" />
+      </RouterLink>
+    </div>
+  </div>
+</template>
