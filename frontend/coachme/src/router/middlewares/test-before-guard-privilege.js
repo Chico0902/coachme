8058f9320e7614016dff0b022a19589bbd63058a
Sangyeop Nam <spearoad15@gmail.com>
2024-01-19 11:08:49 +0900

    feat : 토큰 받아오는 함수, 토큰 파싱하는 함수, 토큰 정보 확인해서 Router에 진입여부 확ㅇ니하는 기능 추가

--- frontend/coachme/src/router/middlewares/test-before-guard-privilege.js
+++ frontend/coachme/src/router/middlewares/test-before-guard-privilege.js
@@ -1,4 +1,4 @@
-import { decodeToken } from '../../utils/functions/auth'
+import { getAccessToken, checkPrivilegeOverDemand } from '../../utils/functions/auth'
 /**
  * 현재 JWT의 권한 정보를 확인해서 권한이 1보다 작으면 입장을 막는 함수
  * @param {route} to 대상 Route 객체로 이동합니다.
@@ -6,17 +6,13 @@ import { decodeToken } from '../../utils/functions/auth'
  * @param {function} next 훅을 해결하기 위한 호출입니다.(참고 : https://v3.router.vuejs.org/kr/guide/advanced/navigation-guards.html)
  */
 export function guardPrivilegeOne(to, from, next) {
-  const token = JSON.parse(sessionStorage.getItem('auth'))
-
-  if (token.accessToken === null) {
-    alert('회원정보가 없습니다. 로그인 페이지로 이동합니다.')
+  try {
+    const token = getAccessToken()
+    const DEMAND = 1
+    checkPrivilegeOverDemand(token, DEMAND)
+    return next()
+  } catch (e) {
+    alert(e.message + ' 로그인 페이지로 이동합니다.')
     return next({ name: 'test-login' })
   }
-  const decodedToken = decodeToken(token.accessToken)
-  const privilege = decodedToken.privilege
-  if (privilege < 1) {
-    alert('권한이 유효하지 않습니다. 로그인 페이지로 이동합니다.')
-    return next({ name: 'test-login' })
-  }
-  return next()
 }
