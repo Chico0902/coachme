2740553a86b7cacf0f11e096a71920d57b60458a
이혜지 <lee02g29@naver.com>
2024-01-22 17:50:15 +0900

    S10P12A403-400 feat : dm 리스트 및 dm 1:1 대화창 기능 추가

--- frontend/coachme/src/components/molecules/DmWindow.vue
+++ frontend/coachme/src/components/molecules/DmWindow.vue
@@ -1,3 +1,10 @@
+<!-- dm 1:1 컴포넌트 
+필요한 내용 : dm 내역, 자신의 아이디, 코치(상대)의 아이디
+chatList : 보낸 사람 id, [채팅 내역]
+myId : 자신의 아이디
+otherId : 코치(상대)의 아이디
+-->
+
 <script setup>
 const props = defineProps({
   chatList: {
@@ -18,9 +25,12 @@ const props = defineProps({
   <div class="q-pa-md row justify-center chat-box">
     <div style="width: 100%; max-width: 400px">
       <div v-for="list in props.chatList" :key="list">
+        <!-- 자신이 보낸 부분-->
         <div v-if="props.myId == list.id">
           <q-chat-message :name="list.id" :text="list.chat" sent bg-color="amber-7"></q-chat-message>
         </div>
+
+        <!-- 상대가 보낸 부분-->
         <div v-else>
           <q-chat-message :name="list.id" :text="list.chat" size="6" text-color="white" bg-color="primary">
           </q-chat-message>
