9928b080c18ddc5ddda98351c30b8ae86149f5a1
남상엽 <spearoad15@gmail.com>
2024-01-23 17:30:18 +0900

    S10P12A403-395-f-전체페이지-라우팅-정리/feat/2

--- frontend/coachme/src/components/molecules/NavBar.vue
+++ frontend/coachme/src/components/molecules/NavBar.vue
@@ -4,11 +4,7 @@ name : 버튼 이름(클래스 이름 같은 것)
 label : nav 버튼에 표시될 이름
 -->
 
-<script setup>
-import buttons from '../atoms/CustomButton.vue'
-const name = ['coach', 'coaching', 'live', 'login', 'logout']
-const label = ['코치찾기', '코칭찾기', '강의장', '로그인', '로그아웃']
-</script>
+<script setup></script>
 
 <template>
   <q-toolbar class="text-black rounded-borders" style="width: 80vw">
@@ -17,9 +13,9 @@ const label = ['코치찾기', '코칭찾기', '강의장', '로그인', '로그
       <img src="../icons/4.png" width="150px" />
     </q-btn>
     <!-- 코치찾기, 코칭찾기, 강의장 -->
-    <buttons flat :name="`${name[0]}`" :label="`${label[0]}`"></buttons>
-    <buttons flat :name="`${name[1]}`" :label="`${label[1]}`"></buttons>
-    <buttons flat :name="`${name[2]}`" :label="`${label[2]}`"></buttons>
+    <slot name="search-coach"> </slot>
+    <slot name="search-coaching"> </slot>
+    <slot name="live"> </slot>
     <q-space></q-space>
 
     <!-- 로그인, 로그아웃, 회원가입, 환영인사 -->
---
+++ frontend/coachme/src/views/search/coach/CoachList.vue
@@ -0,0 +1,4 @@
+<script setup></script>
+<template>
+  <h1>Coach List</h1>
+</template>
