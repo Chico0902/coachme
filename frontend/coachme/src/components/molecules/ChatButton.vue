6b3acafdfda3111c25d168feadf246928bdec3f9
이혜지 <lee02g29@naver.com>
2024-01-24 16:54:15 +0900

    S10P12A403-398 save : 추가한 컴포넌트 및 레이아웃에 주석 추가

--- frontend/coachme/src/components/molecules/ChatButton.vue
+++ frontend/coachme/src/components/molecules/ChatButton.vue
@@ -1,3 +1,5 @@
+<!-- 채팅 버튼 컴포넌트 -->
+
 <script setup>
 import DmList from '.././molecules/DmList.vue'
 import DmWindow from '.././molecules/DmWindow.vue';
@@ -5,9 +7,11 @@ import DmWindow from '.././molecules/DmWindow.vue';
 import { ref } from 'vue'
 
 const isChatList = ref(true)
+// 채팅 목록이 켜져있는지 여부
 
 const myId = "coame"
 const otherId = "coach"
+// 나의 id와 상대의 id
 
 const chat = [{
   "id": "coame",
@@ -19,6 +23,9 @@ const chat = [{
   "id": "coach",
   "chat": ["I just feel like typing a really, really, REALLY long message to annoy you..."]
 }]
+/* 주고받은 dm 내용
+예시로 작성되었으며, 1:1 리스트에 표시될 내역
+*/
 
 
 const dm = [{
@@ -36,29 +43,35 @@ const dm = [{
   name: "White Cat",
   lastDm: "meow",
 }]
+/* dm 리스트
+프로필사진, 이름, 마지막 메시지
+*/
 
-const clickDm = (data) => {
+const clickDm = () => {
   isChatList.value = !isChatList.value
-} 
+} // dm리스트에서 하나의 방을 골랐을 때
 
 const closeDm = () => {
   isChatList.value = !isChatList.value
-}
+} // 1:1dm의 홈버튼을 눌렀을 때
 
 const closeChat = () => {
   isChatList.value = !isChatList.value
-}
+} // 개선 예정
 
 const reset = () => {
   isChatList.value = true
-}
+} // dm 창 자체가 닫혔을 때, 리스트로 돌아가도록 설정
 
 </script>
 
 <template>
+  <!-- dm 버튼 -->
   <q-btn round size="30px" color="amber-7" icon="chat" @click="reset">
+    <!-- dm 영역 -->
     <q-menu style="max-height: 400px; max-width: 400px;">
       <div class="row no-wrap q-pa-md">
+        <!-- dm리스트와 1:1dm을 번갈아가면서 렌더링 -->
         <DmList v-if="isChatList" :dm-list="dm" @click-dm="clickDm"></DmList>
         <DmWindow v-else :chatList="chat" :myId="myId" :otherId="otherId" @close-dm="closeDm" @close-chat="closeChat"></DmWindow>
       </div>
