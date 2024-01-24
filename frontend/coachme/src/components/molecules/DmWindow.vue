19019697831e717e28bca1b6fa9fe08c67403991
이혜지 <lee02g29@naver.com>
2024-01-24 13:44:54 +0900

    S10P12A403-398 feat : 코치 조회 페이지 사이드바 동적 변경 기능 추가

--- frontend/coachme/src/components/molecules/DmWindow.vue
+++ frontend/coachme/src/components/molecules/DmWindow.vue
@@ -6,6 +6,8 @@ otherId : 코치(상대)의 아이디
 -->
 
 <script setup>
+import InputForm from './InputForm.vue';
+
 const props = defineProps({
   chatList: {
     type: Object,
@@ -19,34 +21,48 @@ const props = defineProps({
   }
 })
 
-const emit = defineEmits(['closeDm'])
+const emit = defineEmits(['closeDm'], ['closeChat'])
 
 const closeDm = () => {
   emit('closeDm')
 }
 
+const closeChat = () => {
+  emit('closeChat')
+}
+
 </script>
 
 <template>
-  <div class="q-pa-md row justify-center chat-box">
-    <q-btn flat @click="closeDm">
-    <span class="material-symbols-outlined">
-      Home
-    </span>
-  </q-btn>
-    <div style="width: 100%; max-width: 400px">
-      <div v-for="list in props.chatList" :key="list">
-        <!-- 자신이 보낸 부분-->
-        <div v-if="props.myId == list.id">
-          <q-chat-message :name="list.id" :text="list.chat" sent bg-color="amber-7"></q-chat-message>
-        </div>
+  <div class="q-pa-md row justify-center window-box">
+    <div class="q-pa-md row justify-center chat-box">
+      <q-btn flat @click="closeDm">
+        <span class="material-symbols-outlined">
+          Home
+        </span>
+      </q-btn>
+      <q-btn flat @click="closeChat">
+        <span class="material-symbols-outlined">
+          Close
+        </span>
+      </q-btn>
+      <div style="width: 100%; max-width: 400px">
+        <div v-for="list in props.chatList" :key="list">
+          <!-- 자신이 보낸 부분-->
+          <div v-if="props.myId == list.id">
+            <q-chat-message :name="list.id" :text="list.chat" sent bg-color="amber-7"></q-chat-message>
+          </div>
 
-        <!-- 상대가 보낸 부분-->
-        <div v-else>
-          <q-chat-message :name="list.id" :text="list.chat" size="6" text-color="white" bg-color="primary">
-          </q-chat-message>
+          <!-- 상대가 보낸 부분-->
+          <div v-else>
+            <q-chat-message :name="list.id" :text="list.chat" size="6" text-color="white" bg-color="primary">
+            </q-chat-message>
+          </div>
         </div>
       </div>
+    </div>
+    <div class="input">
+      <InputForm></InputForm>
     </div>
   </div>
 </template>
@@ -54,11 +70,14 @@ const closeDm = () => {
 <style scoped>
 .chat-box {
   background-color: #FFEEC1;
-  max-width: 380px;
+  max-width: 400px;
   max-height: 500px;
 }
 
 .material-symbols-outlined {
   font-size: 25px;
+}
+.input {
+  max-width: 400px;
 }
 </style>
