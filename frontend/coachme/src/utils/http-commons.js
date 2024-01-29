de3995027e0ea628f7b89111571127f77b048227
남상엽 <spearoad15@gmail.com>
2024-01-29 18:02:20 +0900

    S10P12A403-440-f-권한요청-api-구현/feat/2

--- frontend/coachme/src/utils/http-commons.js
+++ frontend/coachme/src/utils/http-commons.js
@@ -3,11 +3,6 @@ import { getAccessToken } from './functions/auth'
 
 const { VITE_BACKEND_URL } = import.meta.env
 let accessToken
-try {
-  accessToken = getAccessToken()
-} catch (e) {
-  console.log('엑세스 토큰이 없습니다.')
-}
 
 function backendAxios() {
   const instance = axios.create({
@@ -20,14 +15,19 @@ function backendAxios() {
 }
 
 function authBackendAxios() {
-  const instance = axios.create({
-    baseURL: VITE_BACKEND_URL,
-    headers: {
-      'Content-Type': 'application/json;charset=utf-8',
-      Authorization: 'Bearer ' + accessToken
-    }
-  })
-  return instance
+  try {
+    accessToken = getAccessToken()
+    console.log(accessToken)
+    return axios.create({
+      baseURL: VITE_BACKEND_URL,
+      headers: {
+        'Content-Type': 'application/json;charset=utf-8',
+        Authorization: 'Bearer ' + accessToken
+      }
+    })
+  } catch (e) {
+    console.log('엑세스 토큰이 없습니다.')
+  }
 }
 
 export { backendAxios, authBackendAxios }
