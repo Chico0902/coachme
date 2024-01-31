e155d60d6f1dc7b369560d896549de3d509e41a5
이혜지 <lee02g29@naver.com>
2024-01-31 17:15:29 +0900

    S10P12A403-403 feat : 코칭 상세 페이지 기능 추가 코치 정보 영역, 코칭 소개 영역, 오른쪽 안내창 기능 추가

---
+++ frontend/coachme/src/components/atoms/CustomBreadCrumbs.vue
@@ -0,0 +1,20 @@
+<script setup>
+const props = defineProps({
+  coachingCategory : {
+    type: Object
+  }
+})
+
+</script>
+
+<template>
+  <q-breadcrumbs class="text-brown">
+    <template v-slot:separator>
+      <q-icon size="1.5em" name="chevron_right" color="primary"></q-icon>
+    </template>
+
+    <q-breadcrumbs-el v-for="menu in props.coachingCategory" :key="menu" :label="menu"></q-breadcrumbs-el>
+  </q-breadcrumbs>
+</template>
+
+<style scoped></style>
