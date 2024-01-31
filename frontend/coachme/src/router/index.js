c7d58b660a52ae13c26dcd528618cca625e52a3a
Sangyeop Nam <spearoad15@gmail.com>
2024-01-31 16:28:19 +0900

    refact : 불필요 test파일 삭제

--- frontend/coachme/src/router/index.js
+++ frontend/coachme/src/router/index.js
@@ -3,11 +3,10 @@ import mainIndex from './indexes/main-index'
 import mypageIndex from './indexes/mypage-index'
 import searchIndex from './indexes/search-index'
 import liveIndex from './indexes/live-index'
-import testIndex from './indexes/test-index'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [...mainIndex, ...mypageIndex, ...searchIndex, ...liveIndex, ...testIndex]
+  routes: [...mainIndex, ...mypageIndex, ...searchIndex, ...liveIndex]
 })
 
 export default router
