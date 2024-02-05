ac5c8a1e1fc196d45e7eb1957a6dbffd5fd7b67a
이혜지 <lee02g29@naver.com>
2024-02-05 17:14:29 +0900

    S10P12A403-476 feat : 코치 라이브 코칭 강의장 기능 추가 화상 회의 레이아웃 추가 채팅, 참가자 목록 기능 추가

--- frontend/coachme/src/components/molecules/LiveChat.vue
+++ frontend/coachme/src/components/molecules/LiveChat.vue
@@ -1,4 +1,4 @@
-<!-- dm 1:1 컴포넌트 
+<!-- 코칭 채팅 컴포넌트 
 필요한 내용 : dm 내역, 자신의 아이디
 directMessage : 보낸 사람 id, [채팅 내역]
 myId : 자신의 아이디
@@ -17,41 +17,21 @@ const props = defineProps({
   }, 
 })
 
-const emit = defineEmits(['closeDm'], ['closeChat'])
-
-const closeDm = () => {
-  emit('closeDm')
-} // 1:1 dm 닫기
-
-const closeChat = () => {
-  emit('closeChat')
-} // dm창 자체를 닫기
+const background = "white"
 
 </script>
 
 <template>
   <div class="q-pa-md row justify-center window-box">
     <div class="q-pa-md row justify-center chat-box">
-      <!-- dm 리스트로 돌아가는 버튼 -->
-      <q-btn flat @click="closeDm">
-        <span class="material-symbols-outlined">
-          Home
-        </span>
-      </q-btn>
-      <!-- dm창을 닫는 버튼 -->
-      <q-btn flat @click="closeChat">
-        <span class="material-symbols-outlined">
-          Close
-        </span>
-      </q-btn>
-      <div style="width: 100%; max-width: 400px">
+      <div style="width: 100%;">
         <div v-for="list in props.directMessage" :key="list">
           <!-- 자신이 보낸 부분-->
           <div v-if="props.myId == list.id">
             <q-chat-message :name="list.id" :text="list.chat" sent bg-color="amber-7"></q-chat-message>
           </div>
 
-          <!-- 상대가 보낸 부분-->
+          <!-- 나 외에 다른 사람이 보낸 부분-->
           <div v-else>
             <q-chat-message :name="list.id" :text="list.chat" size="6" text-color="white" bg-color="primary">
             </q-chat-message>
@@ -60,20 +40,23 @@ const closeChat = () => {
       </div>
     </div>
     <div class="input">
-      <InputForm></InputForm>
+      <InputForm :background="background" bg-color="white"></InputForm>
     </div>
   </div>
 </template>
 
 <style scoped>
+
 .window-box {
-  max-width: min-content;
-  max-height: max-content;
+  height: 70vh;
+  width: 95%;
+  margin: auto;
 }
+
 .chat-box {
-  background-color: #FFEEC1;
-  max-width: 400px;
-  max-height: 500px;
+  width: 100%;
+  height: calc(100% - 60px);
+  background-color: white;
 }
 
 .material-symbols-outlined {
