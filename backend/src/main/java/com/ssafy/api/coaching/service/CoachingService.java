f9ac621c2983c1a5cbcb3c4072c3fe04706565ae
박민아 <minahpark42@gmail.com>
2024-02-05 17:22:16 +0900

    S10 p12 a403 463 b 코칭 조회 api 구현/feat/5

--- backend/src/main/java/com/ssafy/api/coaching/service/CoachingService.java
+++ backend/src/main/java/com/ssafy/api/coaching/service/CoachingService.java
@@ -5,6 +5,10 @@ import com.ssafy.api.coaching.dto.request.CreateCoachingRequestDto;
 import com.ssafy.api.coaching.dto.response.CoachingDetailResponseDto;
 import com.ssafy.api.coaching.dto.response.CoachingResponseDtos;
 import com.ssafy.api.coaching.dto.response.CoameListResponseDto;
+import com.ssafy.api.coach.dto.CoachesCoachingsResponseDto;
+import com.ssafy.api.coaching.dto.CoachingInfoChangeRequestDto;
+import com.ssafy.api.coaching.dto.CreateCoachingRequestDto;
+import com.ssafy.api.coaching.dto.GetOneCoachingResponseDto;
 import com.ssafy.api.coaching.mapper.CoachingMapper;
 import com.ssafy.api.coaching.repository.CategoryRepository;
 import com.ssafy.api.coaching.repository.CoachingRepository;
@@ -13,6 +17,7 @@ import com.ssafy.api.member.repository.MemberRepository;
 import com.ssafy.api.review.repository.ReviewRepository;
 import com.ssafy.db.entity.*;
 import com.ssafy.db.entity.type.CategoryType;
+import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -126,4 +131,52 @@ public class CoachingService {
     return dto;
   }
 
+  /**
+   * 해당 코치의 코칭 1개 조회
+   */
+  @Transactional(readOnly = true)
+  public GetOneCoachingResponseDto getOneCoaching(Long longId, Long id) {
+    Member member = memberRepository.getReferenceById(longId);
+    List<Coaching> coachings = member.getCoachTeachCourses();
+    log.info(String.valueOf(coachings.size()));
+//    Coaching foundCoaching;
+    for(Coaching coaching : coachings) {
+      if(coaching.getId() == id) {
+        return CoachingMapper.instance.coachingToGetOneCoachingResponseDto(coaching);
+      }
+    }
+    throw new EntityNotFoundException();
+  }
+  /**
+   * 해당 코치의 모든 코칭 조회
+   */
+  @Transactional(readOnly = true)
+  public List<CoachesCoachingsResponseDto> getCoachesCoachings(Long longId) {
+    Member member = memberRepository.getReferenceById(longId);
+    List<Coaching> coachings = member.getCoachTeachCourses();
+    List<CoachesCoachingsResponseDto> list = CoachingMapper.instance.coachingToCoachesCoachingsResponseDto(coachings);
+
+    return list;
+  }
+  /**
+   * 해당 코치의 코칭 정보 수정
+   */
+  public void changeCoachingInfo(Long longId, Long id, CoachingInfoChangeRequestDto dto) {
+    Member member = memberRepository.getReferenceById(longId);
+    List<Coaching> coachings = member.getCoachTeachCourses();
+    for(Coaching coaching : coachings) {
+      if(coaching.getId() == id) {
+        Category main = categoryRepository.findByName(dto.getMain());
+        Category sub = categoryRepository.findByName(dto.getSub());
+        coaching.updatedCoaching(main, sub, dto.getName(), dto.getSummary(), dto.getHtmlDocs());
+      }
+    }
+  }
+  /**
+   * 해당 코치의 코칭 삭제
+   */
+  public void deleteCoaching(Long longId, Long id) {
+    Coaching coaching = coachingRepository.getReferenceById(id);
+    coachingRepository.delete(coaching);
+  }
 }
