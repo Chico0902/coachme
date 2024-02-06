67d7ad6847a4d024b03cf6080020c3cf35fc253b
이준학 <0710june@naver.com>
2024-02-06 17:12:18 +0900

    fix: 불필요 파일 삭제

--- frontend/coachme/src/tests/mocks/worker.js
+++
@@ -1,6 +0,0 @@
-import { setupWorker } from 'msw/browser'
-import memberHandler from './handler/member'
-import adminHandler from './handler/admin'
-import authHandler from './handler/auth'
-
-export const worker = setupWorker(...memberHandler, ...adminHandler, ...authHandler)
--- frontend/coachme/src/views/mypages/coach/VideoEditView.vue
+++
@@ -1,3 +0,0 @@
-<template>
-  <h1>Edit Video</h1>
-</template>
