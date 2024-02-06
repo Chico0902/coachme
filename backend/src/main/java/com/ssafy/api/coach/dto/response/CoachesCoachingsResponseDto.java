1459032e923f2e43f78b1c05a63720bcf5c305e3
이준학 <0710june@naver.com>
2024-02-06 17:53:27 +0900

    fix: 코칭 리스트 조회 에러 수정

--- backend/src/main/java/com/ssafy/api/coach/dto/response/CoachesCoachingsResponseDto.java
+++ backend/src/main/java/com/ssafy/api/coach/dto/response/CoachesCoachingsResponseDto.java
@@ -4,9 +4,12 @@ import lombok.Data;
 
 @Data
 public class CoachesCoachingsResponseDto {
+
+  private Long id;
   private String main;
   private String sub;
   private String name;
   private String summary;
   private String htmlDocs;
+  
 }
