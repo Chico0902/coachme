a908191247281e438d214d1f00fc59c2cae64dc5
Jiwon119 <92111247+Jiwon119@users.noreply.github.com>
2024-02-07 08:33:10 +0900

    웹소켓 연동 테스트

--- backend/src/main/java/com/ssafy/config/security/token/JwtAuthenticationFilter.java
+++ backend/src/main/java/com/ssafy/config/security/token/JwtAuthenticationFilter.java
@@ -43,7 +43,8 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
     if ((Arrays.asList(
         "/api/auth/login",
         "/api/members",
-        "/api/members/duplicate/id"
+        "/api/members/duplicate/id",
+            "/api/ws-dm"
     ).contains(request.getRequestURI())) ||
 
         // 특정 api 이름 포함했는지만 확인(동적 매칭)
