b9191c21b89138627e2da10fc803daf61ae3d3ff
이혜지 <lee02g29@naver.com>
2024-02-08 10:23:44 +0900

    feat : 코칭 제목 별 영상 리스트 기능 추가

--- frontend/coachme/src/views/mypages/coach/VideoManageView.vue
+++ frontend/coachme/src/views/mypages/coach/VideoManageView.vue
@@ -3,11 +3,13 @@ import Coaching from '@/components/molecules/CoachingCard.vue'
 import { ref, computed } from 'vue';
 
 const videos = ref([
-  { coachingId: 1, coachingName: "abc", videoId: 1, videoUrl: "https://naver.com", videoName: "a" },
-  { coachingId: 1, coachingName: "abc", videoId: 2, videoUrl: "https://naver.com", videoName: "b" },
-  { coachingId: 2, coachingName: "cde", videoId: 3, videoUrl: "https://naver.com", videoName: "c" },
-  { coachingId: 2, coachingName: "cde", videoId: 4, videoUrl: "https://naver.com", videoName: "d" },
-  { coachingId: 3, coachingName: "fgh", videoId: 5, videoUrl: "https://naver.com", videoName: "e" },
+  { coachingId: 1, coachingName: "이것만 알면 당신도 할 수 있다", videoId: 1, videoUrl: "https://naver.com", videoName: "기본편" },
+  { coachingId: 1, coachingName: "이것만 알면 당신도 할 수 있다", videoId: 2, videoUrl: "https://naver.com", videoName: "연습편" },
+  { coachingId: 1, coachingName: "이것만 알면 당신도 할 수 있다", videoId: 6, videoUrl: "https://naver.com", videoName: "실전편" },
+  { coachingId: 1, coachingName: "이것만 알면 당신도 할 수 있다", videoId: 7, videoUrl: "https://naver.com", videoName: "종합" },
+  { coachingId: 2, coachingName: "김지원의 따뜻한 뜨개질 세계", videoId: 3, videoUrl: "https://naver.com", videoName: "기초" },
+  { coachingId: 2, coachingName: "김지원의 따뜻한 뜨개질 세계", videoId: 4, videoUrl: "https://naver.com", videoName: "프로젝트" },
+  { coachingId: 3, coachingName: "기본부터 진행하는 농구 교실", videoId: 5, videoUrl: "https://naver.com", videoName: "기초편" },
 ]);
 
 const coachingOptions = Array.from(new Set(videos.value.map(video => video.coachingId)))
@@ -16,20 +18,24 @@ const coachingOptions = Array.from(new Set(videos.value.map(video => video.coach
     return { value: matchingVideo.coachingId, label: matchingVideo.coachingName };
   });
 
+const coachingOptionsWithAll = [{ value: null, label: "전체보기" }, ...coachingOptions];
 
 const selectedCoachingId = ref(null);
 
-const filteredVideos = computed(() => {
-  if (selectedCoachingId.value !== null) {
-    return videos.value.filter(video => video.coachingId === selectedCoachingId.value);
-  } else {
-    return videos.value;
-  }
-});
+const groupedFilteredVideos = computed(() => {
+  const filtered = selectedCoachingId.value !== null ?
+    videos.value.filter(video => video.coachingId === selectedCoachingId.value) :
+    videos.value;
 
-const filterVideos = () => {
-  // 선택된 coachingId로 필터링된 동영상 목록 업데이트
-};
+  const grouped = {};
+  filtered.forEach(video => {
+    if (!grouped[video.coachingName]) {
+      grouped[video.coachingName] = [];
+    }
+    grouped[video.coachingName].push(video);
+  });
+  return grouped;
+});
 
 </script>
 <template>
@@ -38,19 +44,18 @@ const filterVideos = () => {
       <div>영상보기</div>
     </div>
     <div class="coaching-dropdown">
-      <q-select v-model="selectedCoachingId" :options="coachingOptions" label="Coaching Name" emit-value map-options
-        @change="filterVideos" />
+      <q-select v-model="selectedCoachingId" :options="coachingOptionsWithAll" 
+      label="코칭 이름" emit-value map-options @change="filterVideos" />
     </div>
 
-    <div class="coaching-outside">
-      <div v-for="video in filteredVideos" :key="video" class="coaching-card">
-        <coaching :label="video.coachingName" :caption="caption" :ratio="ratio" :video="video" :visible="false">
-        </coaching>
+    <div v-for="(videosGroup, coachingName) in groupedFilteredVideos" :key="coachingName" class="card-section">
+      <div class="coaching-group-title">{{ coachingName }}</div>
+      <div class="coaching-outside element-with-scrollbar">
+        <div v-for="video in videosGroup" :key="video.videoId" class="coaching-card">
+          <coaching :label="video.videoName" :caption="caption" :ratio="ratio" :video="video" :visible="false">
+          </coaching>
+        </div>
       </div>
-    </div>
-
-    <div class="pagenation">
-      <div>페네자리</div>
     </div>
   </div>
 </template>
@@ -58,8 +63,9 @@ const filterVideos = () => {
 <style scoped>
 .video-title {
   width: 80%;
-  margin: 5vh auto;
+  margin: 5vh 5vh 1vh;
   font-weight: bolder;
+  font-size: 26px;
 }
 
 .coaching-dropdown {
@@ -69,25 +75,33 @@ const filterVideos = () => {
 
 .coaching-outside {
   width: 100%;
-  height: 30vh;
-  margin: 5vh auto;
+  height: 33vh;
+  margin-top: 0.2vh;
+  margin-bottom: 2vh;
   display: flex;
-  justify-content: center;
+  justify-content: flex-start;
   overflow-x: auto;
-  /* 가로 오버플로우 스크롤 추가 */
   overflow-y: hidden;
-  /* 세로 오버플로우 숨김 */
-  flex-wrap: nowrap;
-  /* 강제로 한 줄에 모든 요소 배치 */
 }
 
 .coaching-card {
-  width: 200px;
-  height: 200px;
-  margin: 30px;
+  margin: 30px 15px 30px;
 }
 
-.pagenation {
-  margin: 5vh;
+.card-section {
+  width: 90%;
+  margin-left: 3vw;
+}
+
+.coaching-group-title {
+  font-size: 20px;
+}
+
+.element-with-scrollbar {
+  overflow: hidden;
+}
+
+.element-with-scrollbar:hover {
+  overflow: auto;
 }
 </style>
