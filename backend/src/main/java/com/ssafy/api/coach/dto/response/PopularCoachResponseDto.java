2f338c4240f762ddc84a2e7ffd9dbd8d363478cc
김지원 <wldnjs35242@naver.com>
2024-02-10 03:01:31 +0900

    feat: 인기 코치 조회 api

---
+++ backend/src/main/java/com/ssafy/api/coach/dto/response/PopularCoachResponseDto.java
@@ -0,0 +1,13 @@
+package com.ssafy.api.coach.dto.response;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class PopularCoachResponseDto {
+  long coachId;
+  String coachProfileImageUrl;
+  String coachName;
+  float coachingReviewAvg;
+}
