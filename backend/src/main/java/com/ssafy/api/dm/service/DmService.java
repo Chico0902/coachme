954cd91d6ef4cd3e594cc97326183c7bae950ea8
김지원 <wldnjs35242@naver.com>
2024-02-10 00:23:01 +0900

    fix : dm 버그 수정

--- backend/src/main/java/com/ssafy/api/dm/service/DmService.java
+++ backend/src/main/java/com/ssafy/api/dm/service/DmService.java
@@ -32,33 +32,6 @@ public class DmService {
   private final RedisUtils redisUtils;
 
   /**
-   * member PK 값으로 해당 멤버가 참여하고 있는 Dm Room 내역 조회
-   *
-   * @param : id (member PK)
-   * @return : DmRoomResponseDto List (long roomId , long coameId, long coachId, LocalDateTime createDate)
-   */
-  public List<DmRoomResponseDto> getDmRoomList(long id) throws Exception {
-    List<DmRoomResponseDto> dmRoomList = new ArrayList<>();
-    List<DMRoom> member1 = memberRepository.getReferenceById(id).getMember1DmRooms();
-    List<DMRoom> member2 = memberRepository.getReferenceById(id).getMember2DmRooms();
-
-    dmRoomList.addAll(DmMapper.instance.dmRoomToDmRoomMember1ResponseDtoList(member1));
-    dmRoomList.addAll(DmMapper.instance.dmRoomToDmRoomMember2ResponseDtoList(member2));
-
-    for (DmRoomResponseDto dmroom : dmRoomList) {
-      String lastDm = redisUtils.getLastDm(dmroom.getRoomId() + "_");
-      if (lastDm.isBlank()) {
-        DM mysqlDm = dmRepository.findByLastDm(dmroom.getRoomId());
-        dmroom.setLastDm(mysqlDm != null ? mysqlDm.getMessage() : lastDm);
-      } else {
-
-        dmroom.setLastDm(lastDm);
-      }
-    }
-    return dmRoomList;
-  }
-
-  /**
    * DmRoom 입장 요청시 (1:1 문의하기 버튼 클릭시)
    * DmRoom이 없는 경우 새로 생성 후 Id return / dmList = null
    * DmRoom이 있는 경우 조회된 Id return      / dmList = 이전 대화 내용 리턴
@@ -91,6 +64,32 @@ public class DmService {
   }
 
   /**
+   * member PK 값으로 해당 멤버가 참여하고 있는 Dm Room 내역 조회
+   *
+   * @param : id (member PK)
+   * @return : DmRoomResponseDto List (long roomId , long coameId, long coachId, LocalDateTime createDate)
+   */
+  public List<DmRoomResponseDto> getDmRoomList(long id) throws Exception {
+    List<DmRoomResponseDto> dmRoomList = new ArrayList<>();
+    List<DMRoom> member1 = memberRepository.getReferenceById(id).getMember1DmRooms();
+    List<DMRoom> member2 = memberRepository.getReferenceById(id).getMember2DmRooms();
+
+    dmRoomList.addAll(DmMapper.instance.dmRoomToDmRoomMember1ResponseDtoList(member1));
+    dmRoomList.addAll(DmMapper.instance.dmRoomToDmRoomMember1ResponseDtoList(member2));
+
+    for (DmRoomResponseDto dmroom : dmRoomList) {
+      String lastDm = redisUtils.getLastDm(dmroom.getRoomId() + "_");
+      if (lastDm == null) {
+        DM mysqlDm = dmRepository.findByLastDm(dmroom.getRoomId());
+        dmroom.setLastDm(mysqlDm != null ? mysqlDm.getMessage() : "");
+      } else {
+        dmroom.setLastDm(lastDm);
+      }
+    }
+    return dmRoomList;
+  }
+
+  /**
    * Dm 내역 조회
    *
    * @param : roomId (DmRoom PK)
@@ -101,18 +100,20 @@ public class DmService {
     DMRoom room = dmRoomRepository.getReferenceById(roomId);
     List<DmListDto> mySqlDmList = DmMapper.instance.DmToDmResponseDto(dmRepository.findByDmRoom(room));
 
+    // Redis에서 읽어온 dm 내역
     List<String[]> redisDmList = redisUtils.getKeysAndValuesStartingWithPrefix(roomId + "_");
     Map<Long, String> memberName = new HashMap<>();
     Map<Long, String> memberProfileUrl = new HashMap<>();
 
+    // Redis DM 목록 처리
     for (int i = 0; i < redisDmList.size(); i++) {
       String key = redisDmList.get(i)[0];
-      System.out.println(key);
       DmRedisDto redis = RedisUtils.parser(key);
       long memberId = redis.getMember();
 
       DmListDto redisToDmList = new DmListDto();
 
+      // 멤버 정보가 없는 경우 추가
       if (!memberName.containsKey(memberId)) {
         Member member = memberRepository.getReferenceById(memberId);
         memberName.put(memberId, member.getName());
@@ -132,72 +133,58 @@ public class DmService {
     return groupDmListByMemberAndTime(mySqlDmList);
   }
 
-
+  // DM 목록을 멤버와 시간에 따라 그룹화하는 메소드
   public static List<DmResponseDto> groupDmListByMemberAndTime(List<DmListDto> dmListDtoList) {
-    Map<Long, Map<String, List<String>>> groupedMap = new HashMap<>();
-    Map<Long, String> memberNameMap = new HashMap<>();
-    Map<Long, String> memberProfileMap = new HashMap<>();
+    // 그룹화된 데이터를 담을 맵
+    Map<String, String> memberInfo = new HashMap<>();
+    List<String> msgList = new ArrayList<>();
+    List<DmResponseDto> dmResponseDtoList = new ArrayList<>();
 
-    Long prevMemberId = null; // 이전 멤버 ID를 추적하기 위한 변수
-    String prevCreateTime = null; // 이전 생성 시간을 추적하기 위한 변수
+    // 이전 멤버 이름과 이전 생성 시간을 추적하기 위한 변수들
+    long prevMemberId = -1;
+    String prevCreateTime = null;
 
-    // 그룹화된 데이터 생성
+    // DM 목록을 순회하며 그룹화
     for (DmListDto dmListDto : dmListDtoList) {
-      Long memberId = dmListDto.getMemberId();
-      String memberName = dmListDto.getMemberName();
-      String memberUrl = dmListDto.getMemberProfileUrl();
+      long memberId = dmListDto.getMemberId();
       String createTime = dmListDto.getCreateDate().truncatedTo(ChronoUnit.MINUTES).toString();
-      List<String> messageList = groupedMap
-          .computeIfAbsent(memberId, k -> new HashMap<>())
-          .computeIfAbsent(createTime, k -> new ArrayList<>());
+      if (memberId != prevMemberId || !createTime.equals(prevCreateTime)) {
+        if (prevMemberId != -1) {
+          // 이전 내역 리스트에 담기
+          DmResponseDto dto = new DmResponseDto();
+          dto.setMemberId(prevMemberId);
+          dto.setMemberName(memberInfo.get("memberName"));
+          dto.setMemberProfileUrl(memberInfo.get("memberProfileUrl"));
+          dto.setCreateDate(memberInfo.get("createDate"));
+          dto.setMessage(msgList);
+          dmResponseDtoList.add(dto);
+        }
 
-      // 연속된 데이터 중에서 ID가 같은 경우에만 분이 같은 것을 묶어줌
-      if (prevMemberId != null && prevMemberId.equals(memberId) && prevCreateTime != null && prevCreateTime.equals(createTime)) {
-        // 연속된 데이터의 경우에만 메시지를 추가
-        messageList.add(dmListDto.getMessage());
+        // 새로운 내역 리스트에 담기
+        memberInfo = new HashMap<>();
+        memberInfo.put("memberName", dmListDto.getMemberName());
+        memberInfo.put("memberProfileUrl", dmListDto.getMemberProfileUrl());
+        memberInfo.put("createDate", dmListDto.getCreateDate().truncatedTo(ChronoUnit.MINUTES).toString());
+
+        msgList = new ArrayList<>();
+        msgList.add(dmListDto.getMessage());
       } else {
-        // 이전 데이터와 ID가 다르거나 분이 다른 경우에는 새로운 리스트 생성
-        messageList = new ArrayList<>();
-        messageList.add(dmListDto.getMessage());
-        groupedMap.get(memberId).put(createTime, messageList);
-
-        // 멤버 이름 및 프로필 사진 정보 설정
-        memberNameMap.put(memberId, memberName);
-        memberProfileMap.put(memberId, memberUrl);
+        msgList.add(dmListDto.getMessage());
       }
-
       prevMemberId = memberId;
       prevCreateTime = createTime;
     }
 
-    // DmResponseDto로 변환
-    List<DmResponseDto> dmResponseDtoList = new ArrayList<>();
-    for (Map.Entry<Long, Map<String, List<String>>> entry : groupedMap.entrySet()) {
-      Long memberId = entry.getKey();
-      String memberName = memberNameMap.get(memberId);
-      String memberProfileUrl = memberProfileMap.get(memberId);
-      Map<String, List<String>> timeAndMessagesMap = entry.getValue();
-
-      for (Map.Entry<String, List<String>> timeAndMessagesEntry : timeAndMessagesMap.entrySet()) {
-        String createDate = timeAndMessagesEntry.getKey();
-        List<String> messages = timeAndMessagesEntry.getValue();
-
-        DmResponseDto dmResponseDto = new DmResponseDto();
-        dmResponseDto.setMemberId(memberId);
-        dmResponseDto.setMemberName(memberName);
-        dmResponseDto.setMemberProfileUrl(memberProfileUrl);
-        dmResponseDto.setCreateDate(createDate);
-        dmResponseDto.setMessage(messages);
-
-        dmResponseDtoList.add(dmResponseDto);
-      }
-    }
-
+    DmResponseDto dto = new DmResponseDto();
+    dto.setMemberId(prevMemberId);
+    dto.setMemberName(memberInfo.get("memberName"));
+    dto.setMemberProfileUrl(memberInfo.get("memberProfileUrl"));
+    dto.setCreateDate(memberInfo.get("createDate"));
+    dto.setMessage(msgList);
+    dmResponseDtoList.add(dto);
 
     return dmResponseDtoList;
   }
-
-
 }
 
 
