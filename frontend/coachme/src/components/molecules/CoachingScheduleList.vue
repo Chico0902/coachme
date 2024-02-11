dbf9c01f428f7c720993e9cc23fdbcc72062c13e
SangyeopNam <spearoad15@gmail.com>
2024-02-11 17:47:37 +0900

    feat : 코미 라이브코칭 신청 기능 완료 - 이제 코미는 코칭 마이페이지에서 수강을 희망하는 라이브코칭을 신청할 수 있음

--- frontend/coachme/src/components/molecules/CoachingScheduleList.vue
+++ frontend/coachme/src/components/molecules/CoachingScheduleList.vue
@@ -1,51 +1,51 @@
-<!-- 코칭 상세 정보 페이지의 날짜별 스케줄 리스트 컴포넌트
-필요한 정보 : 클릭한 날짜, 클릭한 날짜에 배정된 코칭 시간표
-date : 클릭한 날짜. 문자열.
-timeTable : 클릭한 날짜에 배정되어있는 코칭 시간표. Object
--->
-
 <script setup>
+import { getApplyLiveCoaching } from '@/utils/api/coaching-api'
+import { useMemberStore } from '@/stores/member'
 
-const props = defineProps({
-  date: {
-    type: String
-  },
-  timeTable: {
-    type: Object
-  },
-})
+const memberStore = useMemberStore()
+const { longId } = memberStore
+const props = defineProps({ timeTable: Object })
 
-
+const apply = (id) => {
+  console.log(longId)
+  getApplyLiveCoaching(
+    id,
+    longId,
+    (success) => {
+      console.log(success)
+      alert('신청 완료')
+    },
+    (fail) => {
+      console.log(fail)
+      alert('')
+    }
+  )
+}
 </script>
 
 <template>
-  <q-card class="my-card" style="margin-left: 2vw; width: 20vw; max-height: 52vh;">
-    <!-- 날짜 -->
-    <q-card-section style="font-size: 22px;">
-      {{ date }}
-    </q-card-section>
-
+  <q-card class="my-card" style="margin-left: 2vw; width: 20vw; max-height: 52vh">
     <q-card-section>
-      <q-item-label style="font-size: 18px;">시간표</q-item-label>
+      <q-item-label style="font-size: 18px">시간표</q-item-label>
     </q-card-section>
 
     <q-separator spaced inset></q-separator>
 
     <!-- 시간표(배정된 시간 있음) -->
-    <template v-if="props.timeTable.length > 0">
-      <div v-for="(time,index) in props.timeTable" :key="time">
+    <template v-if="props.timeTable != undefined && props.timeTable.length > 0">
+      <div v-for="coaching in props.timeTable" :key="coaching.id">
         <q-list>
           <q-item>
             <q-item-section>
-              <q-item-label>{{ time }}</q-item-label>
+              <q-item-label>{{ coaching.time }}</q-item-label>
             </q-item-section>
 
             <q-item-section side top>
-              <q-btn style="background-color: #FCBF17; color: black;">신청하기</q-btn>
+              <q-btn @click="apply(coaching.id)" style="background-color: #fcbf17; color: black">신청하기</q-btn>
             </q-item-section>
           </q-item>
 
-          <q-separator v-if="index != timeTable.length - 1" spaced inset></q-separator>
+          <q-separator spaced inset></q-separator>
         </q-list>
       </div>
     </template>
@@ -53,14 +53,13 @@ const props = defineProps({
     <!-- 배정된 시간이 없을 때 -->
     <template v-else>
       <q-list>
-          <q-item>
-            <q-item-section>
-              <q-item-label>가능한 시간이 없습니다.</q-item-label>
-            </q-item-section>
-          </q-item>
-        </q-list>
+        <q-item>
+          <q-item-section>
+            <q-item-label>가능한 시간이 없습니다.</q-item-label>
+          </q-item-section>
+        </q-item>
+      </q-list>
     </template>
-    
   </q-card>
 </template>
 
