1debce83ae31440ff6e8469a8985972cae762c8a
minahpark42 <minahpark42@gmail.com>
2024-02-11 17:23:56 +0900

    찜콩 like 1-6까지, member-16 API 완료

--- backend/src/main/java/com/ssafy/api/coaching/repository/LiveCoachingRepository.java
+++ backend/src/main/java/com/ssafy/api/coaching/repository/LiveCoachingRepository.java
@@ -7,22 +7,12 @@ import org.springframework.data.jpa.repository.Query;
 import java.util.List;
 
 public interface LiveCoachingRepository extends JpaRepository<LiveCoaching, Long> {
-
   @Query("SELECT lc FROM LiveCoaching lc " +
-      "JOIN FETCH lc.coaching cc " +
-      "JOIN FETCH cc.coach c " +
-      "WHERE c.longId = ?1")
-  List<LiveCoaching> findByCoachId(Long coachId);
+      "JOIN FETCH lc.coameCoachings cc " +
+      "JOIN FETCH cc.coame cm " +
+      "JOIN FETCH cm.profileImage pi " +
+      "WHERE lc.id = ?1")
+  LiveCoaching findByIdWithDetails(Long liveCoachingId);
 
-  @Query("SELECT lc FROM LiveCoaching lc " +
-      "JOIN FETCH lc.coaching cc " +
-      "JOIN FETCH cc.coach c " +
-      "Join FETCH lc.coameCoachings ccc " +
-      "WHERE ccc.coame.longId = ?1")
-  List<LiveCoaching> findByCoameId(Long coameId);
-
-  @Query("SELECT lc FROM LiveCoaching lc " +
-      "JOIN FETCH lc.coaching cc " +
-      "WHERE cc.id = ?1")
-  List<LiveCoaching> findByCoachingId(Long coachingId);
+  List<LiveCoaching> findByCoameId(Long longId);
 }
