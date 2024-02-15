5fadbdb8a7f8834022aee9a18c8e7dd2db93c242
남상엽 <spearoad15@gmail.com>
2024-02-15 12:17:09 +0900

    화상회의 코치얼굴/feat/2

--- frontend/coachme/src/views/LiveRoom.vue
+++ frontend/coachme/src/views/LiveRoom.vue
@@ -1,12 +1,11 @@
 <script setup>
 import LiveMenuList from '@/components/molecules/LiveMenuList.vue'
-import CoachUserVideo from '@/components/openvidu/UserVideo-coach.vue'
-import CoameUserVideo from '@/components/openvidu/UserVideo-coame.vue'
+import UserVideo from '@/components/openvidu/UserVideo.vue'
 import profile from '@/components/atoms/ProfileImage.vue'
 import LiveChat from '@/components/molecules/LiveChat.vue'
 import router from '@/router'
 import Swal from 'sweetalert2'
-import { ref, onBeforeMount, computed, onBeforeUnmount } from 'vue'
+import { ref, onBeforeMount, computed, onBeforeUnmount, watch } from 'vue'
 import { useRoute } from 'vue-router'
 import { OpenVidu } from 'openvidu-browser'
 import {
@@ -42,7 +41,6 @@ const participants = computed(() => {
   subscribers.value.forEach((subscriber) => ret.push(JSON.parse(subscriber.stream.connection.data).clientData))
   return ret
 }) // 참가자 목록
-const dm = ref([]) // 채팅 목록
 
 // for render
 const isChatOpen = ref(false)
@@ -69,7 +67,8 @@ const screensharing = ref(false)
 const recordingId = ref('')
 
 // for coaching video
-const coachMainVideo = ref(undefined)
+const coachMainStream = ref(undefined)
+const coameStreams = ref([])
 /**
  * METHODS
  */
@@ -163,6 +162,17 @@ const exit = async () => {
   )
 }
 
+// watch(
+//   subscribers,
+//   () => {
+//     // 코치id 찾기, 코치 아닌애들은 코미배열에 넣기
+//     if (subscribers.value.length == undefined) return
+
+//     })
+//   },
+//   { deep: true }
+// )
+
 onBeforeMount(() => {
   // 최초 입장 시 오픈비두 라이브 채팅방에 접속
   console.log(route.params.id)
@@ -178,13 +188,14 @@ onBeforeUnmount(() => {
  * For OpenVidu Session
  */
 
-// function updateScreensharing(value) {
-//   screensharing.value = value
-// }
+function updateScreensharing(value) {
+  screensharing.value = value
+}
 
-// function updatePublisher(value) {
-//   screenPublisher.value = value
-// }
+// 띄워줄 stream이 value로 들어옴!!!
+function updatePublisher(value) {
+  screenPublisher.value = value
+}
 
 function joinSession() {
   // --- 1) Get an OpenVidu object ---
@@ -201,15 +212,16 @@ function joinSession() {
   sessionCamera.value.on('streamCreated', (event) => {
     if (event.stream.typeOfVideo == 'CAMERA') {
       const subscriber = sessionCamera.value.subscribe(event.stream, 'container-cameras')
-      const metadata = JSON.parse(event.target.options.metadata)
-      const clientData = metadata.clientData
-      console.log(clientData)
+      // const metadata = JSON.parse(event.target.options.metadata)
+      // const clientData = metadata.clientData
+      // console.log(clientData)
       subscribers.value.push(subscriber)
+      coameStreams.value = []
       subscribers.value.forEach((subscriber) => {
-        console.log(subscriber)
-        const newId = JSON.parse(subscriber.stream.connection.data)
-        console.log(newId.clientData.id)
-        if (newId.clientData.id == coachId) coachMainVideo.value = subscriber
+        const clientData = JSON.parse(subscriber.stream.connection.data)
+        const userId = clientData.clientData.id
+        if (userId == coachId) coachMainStream.value = subscriber
+        else coameStreams.value.push(subscriber)
       })
     }
   })
@@ -270,7 +282,6 @@ function joinSession() {
 
         // Set the main video in the page to display our webcam and store our Publisher
         mainStreamManager.value = publisher.value
-        coachMainVideo.value = publisher.value
         subscribers.value.push(publisher.value)
 
         // --- 6) Publish your stream ---
@@ -311,11 +322,6 @@ function leaveSession() {
   window.removeEventListener('beforeunload', leaveSession)
 }
 
-function updateMainVideoStreamManager(stream) {
-  if (mainStreamManager.value === stream) return
-  mainStreamManager.value = stream
-}
-
 async function getToken(mySessionId) {
   console.log(getToken)
   const sessionId = await postConnectLiveCoaching({ customSessionId: mySessionId })
@@ -330,117 +336,251 @@ async function getToken(mySessionId) {
   if (inParticipantsFind === undefined) participants.value.push(newParticipant)
   return response.token
 }
+
+/**
+ * FOR TEST!!!!
+ */
+const layoutCount = ref(1)
+const changeLayoutCount = () => {
+  if (layoutCount.value < 4) layoutCount.value++
+  else layoutCount.value = 1
+}
 </script>
 <template>
-  <div class="title">
-    <div>
-      <h5>{{ coachingTitle }}</h5>
-    </div>
-  </div>
-
   <div class="all">
-    <div class="main-layout">
-      <div class="chat-outside">
-        <!-- 코치 화면 -->
-        <div class="coach">
-          <!-- <div style="max-width: 5vw;"> -->
-          <CoachUserVideo id="main-video" :stream-manager="coachMainVideo" />
-          <!-- </div> -->
-        </div>
+    <div class="title">
+      {{ coachingTitle }}
+      <q-btn @click="changeLayoutCount" label="레이아웃 변경"></q-btn>
+      <p>Lay Count = {{ layoutCount }}</p>
+    </div>
 
-        <!-- <div class="person" v-for="sub in subscribers" :key="sub.stream.connection.connectionId">
-          <UserVideo :stream-manager="sub" />
-        </div> -->
-        <!-- 코미 화면 -->
-        <div class="coame-container">
-          <div class="coame element-with-scrollbar">
-            <div v-for="sub in subscribers" :key="sub.stream.connection.connectionId">
-              <CoameUserVideo :stream-manager="sub" />
+    <div class="video-layout">
+      <!-- 코치만 있을때 -->
+      <template v-if="layoutCount === 1">
+        <div class="layout-one">
+          <UserVideo :stream-manager="coachMainStream"></UserVideo>
+        </div>
+      </template>
+
+      <!-- 코치1명 코미 1명 -->
+      <template v-if="layoutCount === 2">
+        <!-- {{ coameStreams }} -->
+        <div class="layout-two">
+          <div class="coach-layout-two"><UserVideo :stream-manager="coachMainStream"></UserVideo></div>
+          <div class="coame-layout-two">
+            <UserVideo :stream-manager="coameStreams[0]"></UserVideo>
+          </div>
+        </div>
+      </template>
+
+      <!-- 코치1명 코미 2명 이상 -->
+      <template v-if="layoutCount === 3">
+        <div class="layout-three">
+          <div class="coach-layout-three">
+            <UserVideo :stream-manager="coachMainStream"></UserVideo>
+          </div>
+          <div class="coame-layout-three">
+            <div class="coame-layout-three-flex">
+              <div class="coame-layout-three-inner"><UserVideo :stream-manager="coameStreams[0]"></UserVideo></div>
+              <div class="coame-layout-three-inner"><UserVideo :stream-manager="coameStreams[1]"></UserVideo></div>
             </div>
           </div>
         </div>
+      </template>
 
-        <!-- 채팅 -->
-        <div v-show="isChatOpen">
-          <q-layout
-            view="lHh Lpr lFf"
-            container
-            style="height: 70vh; width: 30rem"
-            class="shadow-2 rounded-borders dm-window-container"
-          >
-            <q-page-container>
-              <LiveChat :mySessionId="mySessionId" @chatOff="isChatOpen = false"></LiveChat>
-            </q-page-container>
-          </q-layout>
+      <!-- 영상화면 공유 -->
+      <template v-if="layoutCount === 4 && screensharing">
+        <div class="share-layout">
+          <UserVideo :stream-manager="screenPublisher"></UserVideo>
         </div>
+      </template>
+    </div>
 
-        <!-- 참가자 리스트 -->
-        <div v-if="isPeopleOpen" class="people">
-          <template v-for="participant in participants" :key="participant.id">
-            <q-item clickable v-ripple>
-              <q-item-section avatar>
-                <q-item>
-                  <profile :img="participant.imageUrl"></profile>
-                </q-item>
-              </q-item-section>
-              <q-item-section>
-                <q-item-label>{{ participant.memberName }}</q-item-label>
-                <q-item-label caption>{{ participant.profileText }}</q-item-label>
-              </q-item-section>
-            </q-item>
-          </template>
-        </div>
-      </div>
+    <!-- 강의장 메뉴 리스트 -->
+    <div class="button-layout">
+      <LiveMenuList
+        @change-chat-status="changeChatStatus"
+        @change-participants-status="changeParticipantsStatus"
+        @start-record="startRecording"
+        @stop-record="stopRecording"
+        @update-publisher="updatePublisher"
+        @update-screen-sharing="updateScreensharing"
+        @exit="exit"
+        :isCoach="isCoach"
+        :publisher="publisher"
+        :participants="participants"
+        :OVScreen="OVScreen"
+        :sessionScreen="sessionScreen"
+        :screensharing="screenShare"
+      ></LiveMenuList>
     </div>
   </div>
 
-  <!-- 강의장 메뉴 리스트 -->
-  <div class="buttons" style="background-color: #004c98">
-    <LiveMenuList
-      @change-chat-status="changeChatStatus"
-      @change-participants-status="changeParticipantsStatus"
-      @start-record="startRecording"
-      @stop-record="stopRecording"
-      @exit="exit"
-      :isCoach="isCoach"
-      :publisher="publisher"
-      :participants="participants"
-      :OVScreen="OVScreen"
-      :sessionScreen="sessionScreen"
-      :screensharing="screenShare"
-    ></LiveMenuList>
+  <!-- 채팅 -->
+  <div v-show="isChatOpen">
+    <q-layout
+      view="lHh Lpr lFf"
+      container
+      style="height: 70vh; width: 30rem"
+      class="shadow-2 rounded-borders dm-window-container"
+    >
+      <q-page-container>
+        <LiveChat :mySessionId="mySessionId" @chatOff="isChatOpen = false"></LiveChat>
+      </q-page-container>
+    </q-layout>
+  </div>
+
+  <!-- 참가자 리스트 -->
+  <div v-if="isPeopleOpen" class="people">
+    <template v-for="participant in participants" :key="participant.id">
+      <q-item clickable v-ripple>
+        <q-item-section avatar>
+          <q-item>
+            <profile :img="participant.imageUrl"></profile>
+          </q-item>
+        </q-item-section>
+        <q-item-section>
+          <q-item-label>{{ participant.memberName }}</q-item-label>
+          <q-item-label caption>{{ participant.profileText }}</q-item-label>
+        </q-item-section>
+      </q-item>
+    </template>
   </div>
 </template>
 
 <style scoped>
-#main-video {
-  min-width: 100%;
+.all {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
 }
-
 .title {
-  width: 1280px;
-  height: 20px;
+  width: 100%;
+  height: 10vh;
+  background-color: aliceblue;
+  display: flex;
+  justify-content: space-evenly;
+  align-items: center;
+}
+.video-layout {
+  width: 100%;
+  height: 80vh;
+  background-color: #f2af5c;
+}
+.button-layout {
+  background-color: #004c98;
+  width: 100%;
+  height: 10vh;
   text-align: center;
   display: flex;
-  justify-content: flex-start;
+  justify-content: center;
   align-items: center;
-  margin: auto;
-  padding-top: 4.5vh;
 }
-
-.all {
-  display: flex;
-  justify-content: center;
-}
-
-.main-layout {
-  width: 1280px;
-  min-height: calc(88vh - 20px);
-  justify-content: center;
-  align-items: flex-start;
-  overflow-y: auto;
+.layout-one {
   position: relative;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  width: 720px;
+  height: 540px;
+  border: solid 5px #004c98;
+  background-color: #004c98;
 }
+
+.layout-two {
+  position: relative;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  width: 1200px;
+  height: 540px;
+}
+
+.coach-layout-two {
+  border: solid 5px #004c98;
+  background-color: #004c98;
+  position: absolute;
+  top: 50%;
+  left: 5%;
+  transform: translate(0, -50%);
+  width: 480px;
+  height: 360px;
+}
+
+.coame-layout-two {
+  border: solid 5px #004c98;
+  background-color: #004c98;
+  position: absolute;
+  top: 50%;
+  right: 5%;
+  transform: translate(0, -50%);
+  width: 480px;
+  height: 360px;
+}
+
+.layout-three {
+  position: relative;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  width: 1200px;
+  height: 540px;
+}
+
+.coach-layout-three {
+  border: solid 5px #004c98;
+  background-color: #004c98;
+  position: absolute;
+  top: 50%;
+  left: 0;
+  transform: translate(0, -50%);
+  width: 720px;
+  height: 540px;
+}
+
+.coame-layout-three {
+  position: absolute;
+  top: 50%;
+  right: 0;
+  transform: translate(0, -50%);
+  width: 420px;
+  height: 540px;
+}
+.coame-layout-three-flex {
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+}
+.coame-layout-three-inner {
+  border: solid 5px #004c98;
+  background-color: #004c98;
+  width: 320px;
+  height: 240px;
+}
+.share-layout {
+  border: solid 5px #004c98;
+  background-color: #004c98;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  width: 768px;
+  height: 546px;
+}
+
+.people {
+  min-width: 15vw;
+  max-height: 70vh;
+  position: relative;
+  margin-top: 5vh;
+  margin-left: 4vw;
+  border: 3px solid #004c98;
+  background-color: #004c98;
+}
+
 .main-layout::-webkit-scrollbar {
   width: 10px;
   height: 0.5rem;
@@ -459,10 +599,6 @@ async function getToken(mySessionId) {
 .main-layout::-webkit-scrollbar-track {
   background-color: #c7c7c7;
   border-radius: 1.5rem;
-}
-.chat-outside {
-  display: flex;
-  justify-content: space-between;
 }
 
 .coach {
@@ -530,42 +666,5 @@ async function getToken(mySessionId) {
 }
 .coame {
   max-width: 50vw;
-}
-
-.element-with-scrollbar {
-  overflow: scroll;
-}
-
-.element-with-scrollbar:hover {
-  overflow: auto;
-}
-
-.chat {
-  min-width: 24.2vw;
-  max-height: 70vh;
-  justify-content: center;
-  align-items: flex-start;
-  overflow-y: auto;
-  position: relative;
-  margin-top: 5vh;
-  margin-left: 4vw;
-  border-radius: 15px;
-}
-
-.people {
-  min-width: 15vw;
-  max-height: 70vh;
-  position: relative;
-  margin-top: 5vh;
-  margin-left: 4vw;
-  border: 3px solid #fbe5a9;
-}
-
-.buttons {
-  height: 10vh;
-  text-align: center;
-  display: flex;
-  justify-content: center;
-  align-items: center;
 }
 </style>
